777bee97665276d6132ed8befffa318e73e70d8d
SBhupinder <bsingh168@hotmail.com>
2026-03-26 13:11:39 -0400

    updated script file with toggle detials

--- script.js
+++ script.js
@@ -2,4 +2,19 @@ console.log("Script loaded"); //this one is added to show that step 1 is complet
 
 const buttons = document.querySelectorAll(".toggle-btn");
 
-buttons 
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        const card = button.closest(".project-card");
+        const details = card.querySelector(".project-details");
+
+        details.classList.toggle("hidden");
+
+        if (details.classList.contains("hidden")){
+            button.textContent = "Show Details";
+        } else {
+            button.textContent = "Hide Details";
+        }
+    });
+});
+
+    
