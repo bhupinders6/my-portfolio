7cf39033c3ae49c3d29f74c6f421705a7587c4df
SBhupinder <bsingh168@hotmail.com>
2026-03-26 12:51:21 -0400

    updated html and css file

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Script loaded"); //this one is added to show that step 1 is completed and file is linked properly.
+
+const buttons = document.querySelectorAll(".toggle-btn");
+
+buttons 
