d56b91b2956a7df64fb033f005f5234450a2118d
SBhupinder <bsingh168@hotmail.com>
2026-03-26 13:44:49 -0400

    updated script.js

--- script.js
+++ script.js
@@ -17,4 +17,61 @@ buttons.forEach(function(button) {
     });
 });
 
+const contactForm= document.getElementById("contact-form");
+
+if (contactForm) {
+  contactForm.addEventListener("submit", function(event) {
+    event.preventDefault();
     
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+    const messageInput = document.getElementById("message");
+
+    const nameError = document.getElementById("name-error");
+    const emailError = document.getElementById("email-error");
+    const messageError = document.getElementById("message-error");
+    const successMsg = document.getElementById("success-message");
+
+    nameError.textContent= "";
+    emailError.textContent= "";
+    messageError.textContent= "";
+    successMsg.style.display="none";
+
+    let isValid= true;
+
+        if (nameInput.value.trim() === "") {
+
+            nameError.textContent= "Name is required.";
+
+            isValid= false;
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          if (emailInput.value.trim() === "") {
+
+          emailError.textContent = "Email is required.";
+
+          isValid = false;
+
+        } else if (!emailRegex.test(emailInput.value.trim())) {
+
+            emailError.textContent = "Please enter a valid email address.";
+
+            isValid = false;
+        }
+
+        if (messageInput.value.trim() === "") {
+
+            messageError.textContent = "Message cannot be empty";
+            
+            isValid = false;
+            }
+
+        if (isValid) {
+            successMsg.style.display = "block";
+            contactForm.reset();
+        }
+    });
+}
+
+
